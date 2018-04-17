926b653ccae9c2b6512bce048ded40ea9da28e60
Karl Flores <k.flores@student.unimelb.edu.au>
2018-04-17 04:22:52 +0000

    could not get content length to work -- requires more investigation, at this moment in time we ccan leave it out

--- http_library.c
+++ http_library.c
@@ -29,96 +29,12 @@ char *process_get_request(const char *request){
   return file_path;
 
   //remember to free the file_path once finished
-//
 }
 
-/*
-//sends the response to the socket
-int send_response(int sock_fd,const char *file_path,int response){
-  char message[MAX_BUFF_SIZE];
-  int n;
-  if(sock_df < 0){
-    //return FAILED ;
-    return -1;
-  }
-
-  //get file extension
-  char file_path_cpy[strlen(file_path)+1];
-  strcpy(file_path_cpy,file_path);
-
-  char *file_ext;
-  file_ext = strtok(file_path_cpy,".\n ");
-  if(file_ext == NULL){
-    return -1;
-  }
-  //know that the entries after the '.' is the file extension
-  file_ext = strtok(NULL,".\n ");
-
-  if(response == 200){
-    strcpy(message,"HTTP/1.0 200 OK\r\n");
-    if( (n = write(sock_df,message,strlen(message)))<0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-  }else if(response == 404){
-    strcpy(message,"HTTP/1.0 404\r\n");
-    if((n = write(sock_df,message,strlen(message))) < 0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-    //success -- don't need to return the content type
-    return 0;
-  }else{
-    //if not a 200 or 404 return error
-    return -1;
-  }
-
-  //send the content type
-
-  //reset message buffer
-  memset(message,0,sizeof(message));
-
-  //determine the content type
-  if(strcmp(file_ext,"html") == 0){
-    strcpy(message,"Content-Type: text/html\r\n\r\n");
-
-    if((n = write(sock_df,message,strlen(message))) < 0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-
-  }else if(strcmp(file_ext,"css") == 0){
-    strcpy(message,"Content-Type: text/css\r\n\r\n");
-
-    if((n = write(sock_df,message,strlen(message))) < 0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-
-  }else if(strcmp(file_ext,"js") == 0){
-    strcpy(message,"Content-Type: text/javascript\r\n\r\n");
-
-    if((n = write(sock_df,message,strlen(message))) < 0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-
-  }else if(strcmp(file_ext,"jpg") == 0){
-    strcpy(message,"Content-Type: image/jpeg\r\n\r\n");
-
-    if((n = write(sock_df,message,strlen(message))) < 0){
-      perror("ERROR: could not write HTTP response\n");
-      return -1;
-    }
-
-  }
-  //return successs
-  return 0;
-}
-*/
-
-//sends the response to the socket
+//sends the htpp response to the specified socket
 int send_response(struct http_struct *http_response){
+
+  // convert the struct into relavent data entries
 
   int sock_fd = http_response->sock_fd;
   char *file_path = http_response->file_path;
@@ -128,7 +44,7 @@ int send_response(struct http_struct *http_response){
   int n;
   if(sock_fd < 0){
     //return FAILED ;
-    return -1;
+    return FAILURE;
   }
 
   //get file extension
@@ -138,7 +54,7 @@ int send_response(struct http_struct *http_response){
   char *file_ext;
   file_ext = strtok(file_path_cpy,".\n ");
   if(file_ext == NULL){
-    return -1;
+    return FAILURE;
   }
   //know that the entries after the '.' is the file extension
   file_ext = strtok(NULL,".\n ");
@@ -147,33 +63,33 @@ int send_response(struct http_struct *http_response){
     strcpy(message,"HTTP/1.0 200 OK\r\n");
     if( (n = write(sock_fd,message,strlen(message)))<0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
   }else if(response == 404){
     strcpy(message,"HTTP/1.0 404\r\n");
     if((n = write(sock_fd,message,strlen(message))) < 0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
     //success -- don't need to return the content type
-    return 0;
+    return SUCCESS;
   }else{
     //if not a 200 or 404 return error
-    return -1;
+    return FAILURE;
   }
-
-  //send the content type
+  // at this point we know that it is not a 404 therefore must be a 200 request
+  //send the MIME-type: content-type field
 
   //reset message buffer
   memset(message,0,sizeof(message));
 
-  //determine the content type
+  //determine the content type and send the appropriate HTTP response
   if(strcmp(file_ext,"html") == 0){
     strcpy(message,"Content-Type: text/html\r\n\r\n");
 
     if((n = write(sock_fd,message,strlen(message))) < 0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
 
   }else if(strcmp(file_ext,"css") == 0){
@@ -181,7 +97,7 @@ int send_response(struct http_struct *http_response){
 
     if((n = write(sock_fd,message,strlen(message))) < 0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
 
   }else if(strcmp(file_ext,"js") == 0){
@@ -189,7 +105,7 @@ int send_response(struct http_struct *http_response){
 
     if((n = write(sock_fd,message,strlen(message))) < 0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
 
   }else if(strcmp(file_ext,"jpg") == 0){
@@ -197,10 +113,10 @@ int send_response(struct http_struct *http_response){
 
     if((n = write(sock_fd,message,strlen(message))) < 0){
       perror("ERROR: could not write HTTP response\n");
-      return -1;
+      return FAILURE;
     }
 
   }
   //return successs
-  return 0;
+  return SUCCESS;
 }
